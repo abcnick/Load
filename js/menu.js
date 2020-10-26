4a97d8b1ad2f2a2281a46f1a0d4c9b96d6e0047a
abcnick <nicolasgutierreznag@gmail.com>
2020-10-25 21:33:07 -0500

    Segunda fase

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,8 @@
 const menup = document.querySelector('.menup')
-const menup1 = document.querySelector('.menup1')
 const menu = document.querySelector('.menu-navegacion')
 
 menup.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
-})
-menup1.addEventListener('click', ()=>{
-  src="img/pie_chart.svg"
 })
 
 window.addEventListener('click', e =>{
